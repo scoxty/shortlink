fb30d85382eb906fed0302822417f4aea362950e
scoxty <xty1821901@163.com>
2024-07-27 20:49:59 +0800

    feature: 短链接后管开发短链接修改功能

--- admin/src/main/java/com/xty/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/xty/shortlink/admin/controller/ShortLinkController.java
@@ -2,15 +2,14 @@ package com.xty.shortlink.admin.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.xty.shortlink.admin.common.convention.result.Result;
+import com.xty.shortlink.admin.common.convention.result.Results;
 import com.xty.shortlink.admin.remote.dto.ShortLinkRemoteService;
 import com.xty.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.xty.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.xty.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.xty.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.xty.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 短链接后管控制层
@@ -30,6 +29,15 @@ public class ShortLinkController {
     @PostMapping("/api/short-link/admin/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 修改短链接
+     */
+    @PutMapping("/api/short-link/admin/v1/update")
+    public Result<Void> updateShortLink(@RequestBody ShortLinkUpdateReqDTO requestParam) {
+        shortLinkRemoteService.updateShortLink(requestParam);
+        return Results.success();
     }
 
     /**
--- admin/src/main/java/com/xty/shortlink/admin/remote/dto/ShortLinkRemoteService.java
+++ admin/src/main/java/com/xty/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.xty.shortlink.admin.common.convention.result.Result;
 import com.xty.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import com.xty.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.xty.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.xty.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.xty.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.xty.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
@@ -60,5 +61,14 @@ public interface ShortLinkRemoteService {
         String resultGroupCountStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/count", requestMap);
         return JSON.parseObject(resultGroupCountStr, new TypeReference<>() {
         });
+    }
+
+    /**
+     * 修改短短连接
+     *
+     * @param requestParam 修改短链接请求参数
+     */
+    default void updateShortLink(ShortLinkUpdateReqDTO requestParam) {
+        HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/update", JSON.toJSONString(requestParam));
     }
 }
