1423fa217dc6bdf7cc31f64f46c6834a31b69780
scoxty <xty1821901@163.com>
2024-07-28 15:12:15 +0800

    docs: 短链接跳转原始链接

--- project/src/main/java/com/xty/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/com/xty/shortlink/project/controller/ShortLinkController.java
@@ -26,6 +26,9 @@ import java.util.List;
 public class ShortLinkController {
     private final ShortLinkService shortLinkService;
 
+    /**
+     * 短链接跳转原始链接
+     */
     @GetMapping("/{short-uri}")
     public void restoreUrl(@PathVariable("short-uri") String shortUri, ServletRequest request, ServletResponse response) throws IOException {
         shortLinkService.restoreUrl(shortUri, request, response);
